a748a56e47203f455fe6a553604e9108d717caa8
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:16:38 -0600

    Se terminó el ejercicio 6 mayor de edad

---
+++ mayor_Edad.py
@@ -0,0 +1,9 @@
+# Jonathan Gabriel Morales Tores
+# Ejercicio 6: datos booleanos
+
+edad = 17
+
+#Verificación manual sin condicionales
+mayor_de_edad = False  # Asumimos que no
+
+print("¿Es mayor de edad?: ", mayor_de_edad)
