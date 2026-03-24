abc02b25be1395b6e4779a37ec1f3e6fa5c966d1
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:29:36 -0600

    Ejercicio de conversión #2 completo

---
+++ float_a_int.py
@@ -0,0 +1,9 @@
+# Jonathan Gabriel Morales Torres
+# ejercicio 11: convertir de float a int
+
+precio = 19.99
+
+# conversión a entero ( pierde los decimales )
+precio_entero = int(precio)
+
+print("Precio entero: ", precio_entero)
