9821d26e7ddcbc617d5a537cc0047baf481cffa8
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:09:42 -0600

    Cuarto commit, el ejercicio de datos flotanter terminado

---
+++ precioProductos.py
@@ -0,0 +1,14 @@
+# Jonathan Gabriel Morales Torres
+
+# ejercici1cfcr
+
+# Precio de un producto
+precio = 12.50
+
+# Cantidad de productos
+cantidad = 2
+
+# Total a pagar
+total = precio * cantidad
+
+print("Total a pagar: ", total)
