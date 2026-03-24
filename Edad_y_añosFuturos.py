eeebc2353f8c429fb1c03d33a059e7ab34a56754
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:04:35 -0600

    Ejercicio 2 terminado

---
+++ Edad_y_añosFuturos.py
@@ -0,0 +1,14 @@
+#Jonathan Gabriel Morales Torres "10"
+# Ejercici1cfcr
+# El ejercicio agregar sus datos
+# Agregar su nombre
+
+# edad actual
+edad = 17
+
+# Sumar 5 años más
+edad_futuro : edad +5
+
+# Mostrar resultados
+print("Edad actual: ", edad)
+print("Edad en 5 años: ", edad_futuro)
