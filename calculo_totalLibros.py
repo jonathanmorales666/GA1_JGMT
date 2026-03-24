a76026e8ab737186d1d4c7cdf01b56655c67e18d
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:07:12 -0600

    Tercer ejercicio terminado

---
+++ calculo_totalLibros.py
@@ -0,0 +1,16 @@
+# Jonathan Gabriel Morales Torres
+
+# Los ejercicios deben de tener sus iniciales de su nombre completo
+# ejercici1cfcr
+
+# Cantidad de libros en casa
+libros_en_casa = 30
+
+# Libros que se compraron
+libros_comprados = 10
+
+# Total de libros
+
+total_libros = libros_comprados + libros_en_casa
+
+print("Total de libros: ", total_libros)
