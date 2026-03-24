afa3c40d16df9325efe4e63d18c6fb8d94080b47
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:24:51 -0600

    Ejercicio del nombre completo completado, jiji

---
+++ datosString.py
@@ -0,0 +1,9 @@
+# Jonathan Gabriel Morales Torres 10
+# Ejercicio 8: concatenar nombres
+
+nombre = "Jonathan"
+apellido = "Morales"
+
+nombre_completo = nombre + " " + apellido
+
+print("Su nombre completo es: ", nombre_completo)
