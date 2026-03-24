e8935232891b21c5ef765a9222d62a10f54450a8
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:31:27 -0600

    Ejercicio de constante y radio 10 completo

---
+++ constantePython.py
@@ -0,0 +1,12 @@
+# Jonathan Gabriel Morales Torres
+# Ejercicio 12: constante en Python
+
+# el radio debe ser 10
+
+PI =.314159
+
+# Usamos la constante para calculart el área del circulo
+radio = 10
+area = PI * radio * radio
+
+print("Área del círculo es: ", area)
