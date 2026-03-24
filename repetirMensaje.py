40b0ee795f8db87618cff633c4d1f495a2f331e2
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:26:21 -0600

    Ejercicio saludo completo

---
+++ repetirMensaje.py
@@ -0,0 +1,10 @@
+# Jonathan Gabriel Morales Torres
+# ejercicio 9: repetir un mensaje
+
+mensaje = "Analisis de sistemas"
+
+#repetimos 3 veces
+
+saludo = mensaje * 3
+
+print("Saludo repetido: ", saludo)
