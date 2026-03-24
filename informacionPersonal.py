f3b960a2a6abda9c23916a73c46a5732bd26b515
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 09:58:11 -0600

    Mi primer commit, primer ejercicio terminado

---
+++ informacionPersonal.py
@@ -0,0 +1,19 @@
+# Jonathan Gabriel Morales Torres "10"
+
+# 1. Nombre ( Cadena de texto - String)
+nombre = "Jonatnan Morales"
+
+# 2. Edad ( entero - int)
+edad = 17
+
+# 3. Altura en metros ( Flotante - float )
+altura = 1.65
+
+# 4. ¿Es estudiante? ( booleano - bool )
+es_estudiante = True
+
+# Mostramos los valores
+print("Nombre", nombre)
+print("Edad", edad)
+print("Altura", altura)
+print("Estudiante:", es_estudiante)
