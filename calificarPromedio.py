76cd62426121bbea524303edff8a302c0ceef2c3
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:14:05 -0600

    Ejercicio del promedio terminado

---
+++ calificarPromedio.py
@@ -0,0 +1,16 @@
+# Jonathan Gabriel Morales Torres "10"
+# Ejercicio1cfcr
+
+
+
+# Notas de un estudiante
+
+nota1 = 8.5
+nota2 = 9.0
+nota3 = 7.3
+nota4 =6.7
+
+# promedio
+promedio = (nota1+nota2+nota3+nota4)/4
+
+print("Su promedio es de: ", promedio)
