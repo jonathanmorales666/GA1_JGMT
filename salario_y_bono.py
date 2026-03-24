0d8e827472b6ceef4b2d47b1ea48214c11e2758e
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:38:40 -0600

    Ejercicio de calcular salarios y beneficios terminado, todo terminado

---
+++ salario_y_bono.py
@@ -0,0 +1,26 @@
+# Jonathan Gabriel Morales Torres
+salario_base= 3500.00        # Salario mensual en quetzales
+bono_productividad =500.00   # Bono mensualn fijo
+impuesto = 0.12              # 12% de impuesto sobre el salario total
+
+# Datos del empleado ( variables )
+nombre_empleado = "Jonathan Morales"
+es_empleado_fijo = True
+
+#Cálculo del salario bruto ( salario base + bono )
+salario_bruto = salario_base + bono_productividad
+
+#Cálculo del descuento por impuestos
+descuento = salario_bruto * impuesto
+
+# Cálculo del salario neto
+salario_neto = salario_bruto - descuento
+
+# Mostrar información
+print("Nombre del empleado: ", nombre_empleado)
+print("¿Empleadp fijo?: ", es_empleado_fijo)
+print("Salario base:", salario_base)
+print("Bono de productividad:", bono_productividad)
+print("Salario bruto:", salario_bruto)
+print("Descuento por impuesto:", descuento)
+print("Salario neto:", salario_neto)
