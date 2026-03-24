025e7fd82a60a97ad68ffadcb6a255193604111f
Jonathan Morales <jonathanmorales@emilianisomascos.edu.gt>
2026-03-24 10:28:04 -0600

    Ejercicio de conversión completo

---
+++ casting.py
@@ -0,0 +1,9 @@
+# Jonathan Gabriel Morales Torres
+# ejercicio 10: convertir int a float
+
+numero_entero = 10
+
+#Casting de entero a flotante
+numero_flotante = float(numero_entero)
+
+print("Numero flotante: ", numero_flotante)
